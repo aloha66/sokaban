ee25512c4fe5e2225cd57f865a8d3b530c4b53f7
aloha66 <lzcbruce@live.com>
2023-11-13 22:16:18 +0800

    feat: init

---
+++ src/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+import { useMapStore } from '~/store/map';
+
+defineOptions({
+  name: 'IndexPage',
+})
+
+
+const {map} =  useMapStore()
+
+
+</script>
+
+<template>
+  <div>
+ {{ map }}
+  </div>
+</template>
