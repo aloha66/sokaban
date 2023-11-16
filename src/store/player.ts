230d1051cb2cd4897545d454b23a3d05f0119006
aloha66 <lzcbruce@live.com>
2023-11-16 15:57:33 +0800

    feat: add right,up,down should not move when collision a wall

--- src/store/player.ts
+++ src/store/player.ts
@@ -1,29 +1,35 @@
 import { defineStore } from 'pinia'
-import { MapTile, useMapStore } from './map'
+import { useMapStore } from './map'
 
 export const usePlayerStore = defineStore('player', () => {
+  const { isWall } = useMapStore()
+
   const player = reactive({
     x: 1,
     y: 1,
   })
 
   function movePlayerToLeft() {
-    const { isWall } = useMapStore()
-
     if (isWall({ x: player.x - 1, y: player.y }))
       return
     player.x--
   }
 
   function movePlayerToRight() {
+    if (isWall({ x: player.x + 1, y: player.y }))
+      return
     player.x++
   }
 
   function movePlayerToUp() {
+    if (isWall({ x: player.x, y: player.y - 1 }))
+      return
     player.y--
   }
 
   function movePlayerToDown() {
+    if (isWall({ x: player.x, y: player.y + 1 }))
+      return
     player.y++
   }
 
--- src/store/test/player.test.ts
+++ src/store/test/player.test.ts
@@ -83,6 +83,7 @@ describe('player', () => {
         [1, 1, 1, 1, 1, 1, 1, 1],
       ])
     })
+
     it('should not move to left when collision a wall', () => {
       const { player, movePlayerToLeft } = usePlayerStore()
       player.x = 1
@@ -92,6 +93,39 @@ describe('player', () => {
 
       expect(player.x).toBe(1)
       expect(player.y).toBe(1)
+    })
+
+    it('should not move to right when collision a wall', () => {
+      const { player, movePlayerToRight } = usePlayerStore()
+      player.x = 6
+      player.y = 1
+
+      movePlayerToRight()
+
+      expect(player.x).toBe(6)
+      expect(player.y).toBe(1)
+    })
+
+    it('should not move to up when collision a wall', () => {
+      const { player, movePlayerToUp } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToUp()
+
+      expect(player.x).toBe(1)
+      expect(player.y).toBe(1)
+    })
+
+    it('should not move to down when collision a wall', () => {
+      const { player, movePlayerToDown } = usePlayerStore()
+      player.x = 1
+      player.y = 3
+
+      movePlayerToDown()
+
+      expect(player.x).toBe(1)
+      expect(player.y).toBe(3)
     })
   })
 })
