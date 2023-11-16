e83cad57bf0eb5bb84a8a53de0c1f83d726dfc63
aloha66 <lzcbruce@live.com>
2023-11-16 15:26:44 +0800

    refactor: 新增正常移动和碰墙不能移动的测试分类

--- src/store/test/player.test.ts
+++ src/store/test/player.test.ts
@@ -8,70 +8,90 @@ describe('player', () => {
     setActivePinia(createPinia())
   })
 
-  // 独居测试向左移动
-  it('should move to left', () => {
-    const { setupMap } = useMapStore()
+  describe('normal move', () => {
+    beforeEach(() => {
+      const { setupMap } = useMapStore()
+      /**
+       * 跟墙（其他数据）有所关联
+       * 需要自备测试数据
+       */
+      setupMap([
+        [2, 2, 2],
+        [2, 2, 2],
+        [2, 2, 2],
+      ])
+    })
 
-    setupMap([
-      [2, 2, 2],
-      [2, 2, 2],
-      [2, 2, 2],
-    ])
-    const { player, movePlayerToLeft } = usePlayerStore()
-    player.x = 1
-    player.y = 1
+    // 独居测试向左移动
+    it('should move to left', () => {
+      const { player, movePlayerToLeft } = usePlayerStore()
+      player.x = 1
+      player.y = 1
 
-    movePlayerToLeft()
+      movePlayerToLeft()
 
-    expect(player.x).toBe(0)
-    expect(player.y).toBe(1)
+      expect(player.x).toBe(0)
+      expect(player.y).toBe(1)
+    })
+
+    it('should move to right', () => {
+      const { player, movePlayerToRight } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToRight()
+
+      expect(player.x).toBe(2)
+      expect(player.y).toBe(1)
+    })
+
+    it('should move to up', () => {
+      const { player, movePlayerToUp } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToUp()
+
+      expect(player.x).toBe(1)
+      expect(player.y).toBe(0)
+    })
+
+    it('should move to down', () => {
+      const { player, movePlayerToDown } = usePlayerStore()
+      player.x = 1
+      player.y = 1
+
+      movePlayerToDown()
+
+      expect(player.x).toBe(1)
+      expect(player.y).toBe(2)
+    })
   })
 
-  it('should move to right', () => {
-    const { player, movePlayerToRight } = usePlayerStore()
-    player.x = 1
-    player.y = 1
+  describe('collision wall', () => {
+    beforeEach(() => {
+      const { setupMap } = useMapStore()
+      /**
+       * 跟墙（其他数据）有所关联
+       * 需要自备测试数据
+       */
+      setupMap([
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1],
+        [1, 2, 2, 2, 2, 2, 2, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+      ])
+    })
+    it('should not move to left when collision a wall', () => {
+      const { player, movePlayerToLeft } = usePlayerStore()
+      player.x = 1
+      player.y = 1
 
-    movePlayerToRight()
+      movePlayerToLeft()
 
-    expect(player.x).toBe(2)
-    expect(player.y).toBe(1)
-  })
-
-  it('should move to up', () => {
-    const { player, movePlayerToUp } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-
-    movePlayerToUp()
-
-    expect(player.x).toBe(1)
-    expect(player.y).toBe(0)
-  })
-
-  it('should move to down', () => {
-    const { player, movePlayerToDown } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-
-    movePlayerToDown()
-
-    expect(player.x).toBe(1)
-    expect(player.y).toBe(2)
-  })
-
-  /**
-   * 跟墙（其他数据）有所关联
-   * 需要自备测试数据
-   */
-  it('should not move to left when collision a wall', () => {
-    const { player, movePlayerToLeft } = usePlayerStore()
-    player.x = 1
-    player.y = 1
-
-    movePlayerToLeft()
-
-    expect(player.x).toBe(1)
-    expect(player.y).toBe(1)
+      expect(player.x).toBe(1)
+      expect(player.y).toBe(1)
+    })
   })
 })
