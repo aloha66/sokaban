fafa4b49ddf8160e67f7920dbe63723f2ab0cc15
aloha66 <lzcbruce@live.com>
2023-11-23 22:55:20 +0800

    reactor: setupLevel

--- src/store/game.ts
+++ src/store/game.ts
@@ -24,29 +24,7 @@ export const useGameStore = defineStore('game', () => {
     game.isGameCompleted = cargos.every(cargo => cargo.onTarget)
   }
 
-  function setupGame(gameData: GameData) {
-    _gameData = gameData
-    const levelGameData = gameData[game.level - 1]
-    const { player } = usePlayerStore()
-    const { setupMap } = useMapStore()
-    const { createCargo, addCargo, clearAllCargo } = useCargoStore()
-    const { createTarget, addTarget } = useTargetStore()
-
-    clearAllCargo()
-
-    player.x = levelGameData.player.x
-    player.y = levelGameData.player.y
-
-    setupMap(levelGameData.map)
-
-    levelGameData.cargos.forEach(cargo => addCargo(createCargo(cargo)))
-
-    levelGameData.targets.forEach(target => addTarget(createTarget(target)))
-  }
-
-  function toNextLevel() {
-    game.level++
-
+  function setupLevel() {
     const levelGameData = _gameData[game.level - 1]
     const { player } = usePlayerStore()
     const { setupMap } = useMapStore()
@@ -64,6 +42,17 @@ export const useGameStore = defineStore('game', () => {
     levelGameData.cargos.forEach(cargo => addCargo(createCargo(cargo)))
 
     levelGameData.targets.forEach(target => addTarget(createTarget(target)))
+  }
+
+  function setupGame(gameData: GameData) {
+    _gameData = gameData
+    setupLevel()
+  }
+
+  function toNextLevel() {
+    game.level++
+
+    setupLevel()
   }
 
   return {
