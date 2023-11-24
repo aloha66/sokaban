a2979636ce6a33613b73e59b061f482f5aa5397f
aloha66 <lzcbruce@live.com>
2023-11-24 15:16:33 +0800

    chore: remove moveTarget

--- src/store/target.ts
+++ src/store/target.ts
@@ -24,28 +24,12 @@ export const useTargetStore = defineStore('target', () => {
     return targets.find(target => target.x === position.x && target.y === position.y)
   }
 
-  function moveTarget(target: Target, dx: number, dy: number) {
-    const { isWall } = useMapStore()
-    const position = { x: target.x + dx, y: target.y + dy }
-
-    if (isWall(position))
-      return false
-
-    if (findTarget(position))
-      return
-
-    target.x += dx
-    target.y += dy
-    return true
-  }
-
   function clearAllTarget() {
     targets.splice(0, targets.length)
   }
 
   return {
     clearAllTarget,
-    moveTarget,
     findTarget,
     addTarget,
     createTarget,
