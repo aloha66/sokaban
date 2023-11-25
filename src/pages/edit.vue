fb2a901b3f70ada502122c3f0b79086bdbf9dff6
aloha66 <lzcbruce@live.com>
2023-11-25 10:49:54 +0800

    feat: add edit page

---
+++ src/pages/edit.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+
+defineOptions({
+  name: 'Edit',
+})
+
+
+
+
+</script>
+
+<template>
+  <div>
+    <div flex>
+      <div w-2xl bg-pink>地图编辑器</div>
+      <div>数据展示区</div>
+    </div>
+    <div>
+      元素选择区
+    </div>
+  </div>
+</template>
