0d2afee0e9bb44f41e65e0a942e9439de367a185
aloha66 <lzcbruce@live.com>
2023-11-25 21:54:26 +0800

    test: add editElement

---
+++ src/store/edit/test/editElement.test.ts
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { useMapEditStore } from '../mapEdit'
+import { floorEditElement, useEditElementStore, wallEditElement } from '../editElement'
+
+describe('editElement', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  it('should change to wall when current selected element is wall', () => {
+    const { map } = useMapEditStore()
+    const { setCurrentSelectedEditElement, getCurrentSelectedEditElement } = useEditElementStore()
+
+    // 多态
+    setCurrentSelectedEditElement(wallEditElement)
+
+    getCurrentSelectedEditElement().execute({ x: 1, y: 1 })
+
+    expect(map[1][1]).toBe(MapTile.WALL)
+  })
+
+  it('should change to floor when current selected element is floor', () => {
+    const { map } = useMapEditStore()
+    const { setCurrentSelectedEditElement, getCurrentSelectedEditElement } = useEditElementStore()
+
+    // 多态
+    setCurrentSelectedEditElement(floorEditElement)
+
+    getCurrentSelectedEditElement().execute({ x: 1, y: 1 })
+
+    expect(map[1][1]).toBe(MapTile.FLOOR)
+  })
+})
