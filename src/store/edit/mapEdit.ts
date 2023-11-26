87587346adea7f87d31e50d1f939746684fbefa8
aloha66 <lzcbruce@live.com>
2023-11-26 22:15:33 +0800

    feat: add should init map

--- src/store/edit/mapEdit.ts
+++ src/store/edit/mapEdit.ts
@@ -1,14 +1,31 @@
 import { defineStore } from 'pinia'
 
+// @ts-expect-error let me do it
+type MapEdit = MapTile[][]
+
 export const useMapEditStore = defineStore('map-edit', () => {
-  const map = reactive([
-    [2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2],
-    [2, 2, 2, 2, 2, 2],
+  const map = reactive<MapEdit>([
+    // [2, 2, 2, 2, 2, 2],
+    // [2, 2, 2, 2, 2, 2],
+    // [2, 2, 2, 2, 2, 2],
+    // [2, 2, 2, 2, 2, 2],
   ])
 
+  const row = 8
+  const col = 8
+
+  function initMap() {
+    for (let i = 0; i < row; i++) {
+      const cells = []
+      for (let j = 0; j < col; j++)
+        cells.push(MapTile.FLOOR)
+
+      map.push(cells)
+    }
+  }
+
   return {
+    initMap,
     map,
   }
 })
---
+++ src/store/edit/test/mapEdit.test.ts
@@ -0,0 +1,20 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+import { useMapEditStore } from '../mapEdit'
+
+describe('mapEdit', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+  it('should init map', () => {
+    const { map, initMap } = useMapEditStore()
+
+    const row = 8
+    const col = 8
+
+    initMap()
+
+    expect(map.length).toBe(row)
+    expect(map[0].length).toBe(col)
+  })
+})
