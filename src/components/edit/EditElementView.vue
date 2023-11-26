c0b9979394e491df8c8f6bfe3239418f6a117249
aloha66 <lzcbruce@live.com>
2023-11-26 23:25:14 +0800

    feat: add ui updateMapRow

--- src/components/edit/EditElementView.vue
+++ src/components/edit/EditElementView.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 import { floorEditElement, wallEditElement } from '~/store/edit/editElement'
-import { useMapEditStore } from '~/store/edit/mapEdit';
+import { useMapEditStore } from '~/store/edit/mapEdit'
 
-const {row,col,initMap} = useMapEditStore()
+const { initMap, updateMapRow } = useMapEditStore()
+// 补回响应式数据
+const { row, col } = toRefs(useMapEditStore())
 
 initMap()
+
+watchEffect(() => {
+  updateMapRow()
+})
 </script>
 
 <template>
   <div>
     <h3>元素选择区</h3>
     <div m-2 space-y-2>
-      <div>row: <input border border-blue-50 v-model="row"></div>
-      <div>row: <input border border-blue-50 v-model="col"></div>
+      <div>row: <input v-model="row" border border-blue-50></div>
+      <div>row: <input v-model="col" border border-blue-50></div>
     </div>
     <div m-2 flex space-x-2>
       <h4>地图</h4>
