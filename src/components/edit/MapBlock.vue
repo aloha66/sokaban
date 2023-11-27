edbf8e6a2bbf8cfed9b1d426ade30f2db2b16386
aloha66 <lzcbruce@live.com>
2023-11-27 20:14:31 +0800

    feat: add draggable element

--- src/components/edit/MapBlock.vue
+++ src/components/edit/MapBlock.vue
@@ -14,18 +14,36 @@ const props = defineProps<Props>()
 
 const { getCurrentSelectedEditElement } = useEditElementStore()
 
+const { isDragging, startDrag, stopDrag } = useDrag()
+
 function handleClick() {
   getCurrentSelectedEditElement().execute(props)
+}
+
+function handleMouseUp() {
+  stopDrag()
+  window.removeEventListener('mouseup', handleMouseUp)
+}
+
+function handleMouseDown() {
+  startDrag()
+  window.addEventListener('mouseup', handleMouseUp)
+}
+
+function handleMouseMove() {
+  if (!isDragging())
+    return
+  getCurrentSelectedEditElement()?.execute(props)
 }
 </script>
 
 <template>
-  <div border border-white @click="handleClick">
+  <div border border-white @click="handleClick" @mousedown="handleMouseDown" @mousemove="handleMouseMove">
     <template v-if="props.col === MapTile.WALL">
-      <img :src="wallImg">
+      <img :src="wallImg" :draggable="false">
     </template>
     <template v-else-if="MapTile.FLOOR">
-      <img :src="floorImg">
+      <img :src="floorImg" :draggable="false">
     </template>
   </div>
 </template>
