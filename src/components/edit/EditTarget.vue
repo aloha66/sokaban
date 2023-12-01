602b55f971a2d852f210b3b3ae946d113bee8120
aloha66 <lzcbruce@live.com>
2023-12-01 15:35:10 +0800

    feat: add dblcilck target ui

--- src/components/edit/EditTarget.vue
+++ src/components/edit/EditTarget.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import targetImg from '~/assets/target.png'
+import { useEditTargetStore } from '~/store/edit/editTarget'
 import type { Target } from '~/store/target'
 
 const props = defineProps<Target>()
 
 const { position } = usePosition(props, STEP_EDIT)
+
+const { removeTarget } = useEditTargetStore()
+
+function handleDblclick() {
+  removeTarget(props)
+}
 </script>
 
 <template>
-  <div absolute left-0 top-0 :style="`transform: translate3d(${position.left},${position.top},0);`">
+  <div absolute left-0 top-0 :style="`transform: translate3d(${position.left},${position.top},0);`" @dblclick="handleDblclick">
     <img :src="targetImg">
   </div>
 </template>
